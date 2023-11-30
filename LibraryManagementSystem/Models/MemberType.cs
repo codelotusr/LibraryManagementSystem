5fa99c0bd2251313deb0c672c986546062fc70cc
codelotus <95312169+codelotusr@users.noreply.github.com>
2023-11-30 23:29:00 +0200

    MemberType table

--- LibraryManagementSystem/Models/MemberType.cs
+++ LibraryManagementSystem/Models/MemberType.cs
@@ -11,13 +11,37 @@ namespace LibraryManagementSystem.Models
     {
         [Key]
         public int MemberTypeId { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Index(IsUnique = true)]
         public string MemberTypeName { get; set; }
+        [Required]
+        [Range(0, 100)]
         public int MaxBooks { get; set; }
+        [Required]
+        [Range(0, 90)]
         public int MaxDays { get; set; }
+        [Required]
+        [Range(0, 100)]
         public int MaxReservations { get; set; }
+        [Required]
+        [Range(0, 15)]
         public int MaxRenewals { get; set; }
+        [Required]
+        [Range(0, 100)]
         public int MaxReviews { get; set; }
+        [Required]
+        [Range(0, 100)]
         public int MaxEvents { get; set; }
+        [Required]
+        [Range(0, 100)]
         public int MaxRegistrations { get; set; }
+
+        public virtual ICollection<Member> Members { get; set; }
+
+        public MemberType()
+        {
+            Members = new HashSet<Member>();
+        }
     }
 }
