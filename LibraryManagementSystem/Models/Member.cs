19979efdd5f8987fb9d64063a0d3ad6c2d79f64b
codelotus <95312169+codelotusr@users.noreply.github.com>
2023-11-30 23:18:26 +0200

    Member table

--- LibraryManagementSystem/Models/Member.cs
+++ LibraryManagementSystem/Models/Member.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,40 @@ namespace LibraryManagementSystem.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public string Password { get; set; }
 
-        public List<Review> Reviews { get; set; }
-        public List<EventRegistration> EventRegistrations { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(100)]
+        [Index(IsUnique = true)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string PasswordHash { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+        public virtual ICollection<EventRegistration> EventRegistrations { get; set; }
+        public virtual ICollection<BorrowingRecord> BorrowingRecords { get; set; } 
+
+        public Member()
+        {
+            Reviews = new HashSet<Review>();
+            EventRegistrations = new HashSet<EventRegistration>();
+            BorrowingRecords = new HashSet<BorrowingRecord>(); 
+        }
     }
+
 }
