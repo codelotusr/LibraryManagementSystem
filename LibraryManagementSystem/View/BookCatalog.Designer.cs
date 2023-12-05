ea19ceaa5d0a01bffddb74198a8b54273fc67945
codelotus <95312169+codelotusr@users.noreply.github.com>
2023-12-05 14:37:28 +0200

    update

--- LibraryManagementSystem/View/BookCatalog.Designer.cs
+++ LibraryManagementSystem/View/BookCatalog.Designer.cs
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.catalogListView = new System.Windows.Forms.ListView();
+            this.rentABookButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // catalogListView
+            // 
+            this.catalogListView.HideSelection = false;
+            this.catalogListView.Location = new System.Drawing.Point(12, 35);
+            this.catalogListView.Name = "catalogListView";
+            this.catalogListView.Size = new System.Drawing.Size(347, 386);
+            this.catalogListView.TabIndex = 0;
+            this.catalogListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // rentABookButton
+            // 
+            this.rentABookButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rentABookButton.Location = new System.Drawing.Point(517, 217);
+            this.rentABookButton.Name = "rentABookButton";
+            this.rentABookButton.Size = new System.Drawing.Size(110, 42);
+            this.rentABookButton.TabIndex = 3;
+            this.rentABookButton.Text = "Rent";
+            this.rentABookButton.UseVisualStyleBackColor = true;
+            this.rentABookButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(431, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(384, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Title:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(367, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Author:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(431, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(334, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // BookCatalog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rentABookButton);
+            this.Controls.Add(this.catalogListView);
+            this.Name = "BookCatalog";
             this.Text = "BookCatalog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView catalogListView;
+        private System.Windows.Forms.Button rentABookButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- LibraryManagementSystem/View/BookCatalog.cs
+++ LibraryManagementSystem/View/BookCatalog.cs
@@ -16,5 +16,20 @@ namespace LibraryManagementSystem.View
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
